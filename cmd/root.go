f7f7e352c5324aa979fda732083af412c676c2a8
everton_goncalves <everton.goncalves.89@gmail.com>
2025-04-07 14:03:42 -0300

    refactor: http stress

--- cmd/root.go
+++ cmd/root.go
@@ -2,12 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"net/http"
 	"os"
-	"strings"
-	"sync"
-	"time"
 
+	"github.com/egon89/go-stress-test/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -20,11 +17,6 @@ var (
 	body        string
 )
 
-type result struct {
-	statusCode int
-	duration   time.Duration
-}
-
 var rootCmd = &cobra.Command{
 	Use:   "go-stress-test",
 	Short: "A CLI for stress testing APIs",
@@ -32,25 +24,13 @@ var rootCmd = &cobra.Command{
 It allows you to specify the target URL, HTTP method, number of requests, concurrency level, and request body.
 This tool is useful for performance testing and benchmarking your APIs.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if url == "" {
-			fmt.Println("Error: --url is required")
+		err := internal.ValidateInputHttpStress(url, method, requests, concurrency, intervalSec)
+		if err != nil {
+			fmt.Println("Error:", err)
 			return
 		}
 
-		validMethods := map[string]bool{
-			"GET":    true,
-			"POST":   true,
-			"PUT":    true,
-			"DELETE": true,
-		}
-
-		method = strings.ToUpper(method)
-		if !validMethods[method] {
-			fmt.Printf("Invalid HTTP method: %s\nAllowed methods are: GET, POST, PUT, DELETE\n", method)
-			return
-		}
-
-		stressTest(url, method, requests, concurrency, intervalSec, body)
+		internal.HttpStress(url, method, requests, concurrency, intervalSec, body)
 	},
 }
 
@@ -63,7 +43,7 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&url, "url", "u", "", "Target URL (required)")
-	rootCmd.PersistentFlags().StringVarP(&method, "method", "X", "GET", "HTTP method: GET, POST, PUT, DELETE")
+	rootCmd.PersistentFlags().StringVarP(&method, "method", "X", "GET", "HTTP method: GET, HEAD, PATCH, POST, PUT, DELETE")
 	rootCmd.PersistentFlags().IntVarP(&requests, "requests", "r", 10, "Number of requests")
 	rootCmd.PersistentFlags().IntVarP(&concurrency, "concurrency", "c", 1, "Number of concurrent requests")
 	rootCmd.PersistentFlags().IntVarP(&intervalSec, "interval", "i", 0, "Interval between requests in seconds")
@@ -72,96 +52,4 @@ func init() {
 	rootCmd.RegisterFlagCompletionFunc("method", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return []string{"GET", "POST", "PUT", "DELETE"}, cobra.ShellCompDirectiveDefault
 	})
-}
-
-func stressTest(url, method string, totalRequests, concurrentRequests, intervalSec int, body string) {
-	fmt.Println("Starting stress test...")
-	fmt.Printf("URL: %s, Method: %s, Requests: %d, Concurrency: %d\n", url, method, totalRequests, concurrentRequests)
-	if body != "" {
-		fmt.Printf("Body: %s\n", body)
-	}
-
-	var wg sync.WaitGroup
-	semaphore := make(chan struct{}, concurrentRequests)
-	resultsChannel := make(chan result, totalRequests)
-
-	startTime := time.Now()
-	statusCodeCounter := make(map[int]int)
-	var totalRequestDuration time.Duration
-	done := make(chan struct{})
-
-	// goroutine to collect results
-	go func() {
-		for res := range resultsChannel {
-			statusCodeCounter[res.statusCode]++
-			totalRequestDuration += res.duration
-		}
-		close(done) // close the done channel when all results are collected
-	}()
-
-	for i := 0; i < totalRequests; i++ {
-		wg.Add(1)               // increment the wait group counter
-		semaphore <- struct{}{} // acquire a semaphore slot
-
-		// start a goroutine for each request
-		go func(i int) {
-			defer wg.Done() // decrement the wait group counter when the goroutine completes
-			defer func(intervalSec int) {
-				if intervalSec > 0 {
-					time.Sleep(time.Duration(intervalSec) * time.Second)
-				}
-				<-semaphore // release the semaphore slot
-			}(intervalSec)
-
-			start := time.Now()
-
-			client := &http.Client{}
-			reqBody := strings.NewReader(body)
-			req, err := http.NewRequest(strings.ToUpper(method), url, reqBody)
-			if err != nil {
-				fmt.Printf("request %d failed: %v\n", i+1, err)
-				resultsChannel <- result{statusCode: -1, duration: time.Since(start)}
-				return
-			}
-
-			if body != "" {
-				req.Header.Set("Content-Type", "application/json")
-			}
-
-			resp, err := client.Do(req)
-			elapsed := time.Since(start)
-
-			if err != nil {
-				fmt.Printf("request %d failed: %v\n", i+1, err)
-				resultsChannel <- result{statusCode: -1, duration: elapsed}
-				return
-			}
-			defer resp.Body.Close()
-
-			fmt.Printf("Request %d: Status Code: %d (%v)\n", i+1, resp.StatusCode, elapsed)
-
-			resultsChannel <- result{statusCode: resp.StatusCode, duration: elapsed}
-		}(i)
-	}
-
-	wg.Wait()             // wait for all requests to finish
-	close(resultsChannel) // close the results channel to signal completion
-	<-done                // wait for the results collection to finish
-
-	totalDuration := time.Since(startTime)
-	avgDuration := time.Duration(0)
-	if totalRequests > 0 {
-		avgDuration = totalRequestDuration / time.Duration(totalRequests)
-	}
-
-	fmt.Println("\n--- Summary Report ---")
-	for code, count := range statusCodeCounter {
-		if code == -1 {
-			fmt.Printf("Failed requests: %d\n", count)
-		} else {
-			fmt.Printf("Status %d: %d responses\n", code, count)
-		}
-	}
-	fmt.Printf("Total time for %d requests: %v\n", totalRequests, totalDuration)
-	fmt.Printf("Average response time: %v\n", avgDuration)
 }
---
+++ internal/http_stress.go
@@ -0,0 +1,152 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+type result struct {
+	statusCode int
+	duration   time.Duration
+}
+
+func HttpStress(url, method string, totalRequests, concurrentRequests, intervalSec int, body string) {
+	fmt.Println("Starting stress test...")
+	fmt.Printf("URL: %s, Method: %s, Requests: %d, Concurrency: %d\n", url, method, totalRequests, concurrentRequests)
+	if body != "" {
+		fmt.Printf("Body: %s\n", body)
+	}
+
+	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, concurrentRequests)
+	resultsChannel := make(chan result, totalRequests)
+
+	startTime := time.Now()
+	statusCodeCounter := make(map[int]int)
+	var totalRequestDuration time.Duration
+	done := make(chan struct{})
+
+	// goroutine to collect results
+	go func() {
+		for res := range resultsChannel {
+			statusCodeCounter[res.statusCode]++
+			totalRequestDuration += res.duration
+		}
+		close(done) // close the done channel when all results are collected
+	}()
+
+	for i := 0; i < totalRequests; i++ {
+		wg.Add(1)               // increment the wait group counter
+		semaphore <- struct{}{} // acquire a semaphore slot
+
+		// start a goroutine for each request
+		go func(i int) {
+			defer wg.Done() // decrement the wait group counter when the goroutine completes
+			defer func(intervalSec int) {
+				if intervalSec > 0 {
+					time.Sleep(time.Duration(intervalSec) * time.Second)
+				}
+				<-semaphore // release the semaphore slot
+			}(intervalSec)
+
+			start := time.Now()
+
+			client := &http.Client{}
+			reqBody := strings.NewReader(body)
+			req, err := http.NewRequest(strings.ToUpper(method), url, reqBody)
+			if err != nil {
+				fmt.Printf("request %d failed: %v\n", i+1, err)
+				resultsChannel <- result{statusCode: -1, duration: time.Since(start)}
+				return
+			}
+
+			if body != "" {
+				req.Header.Set("Content-Type", "application/json")
+			}
+
+			resp, err := client.Do(req)
+			elapsed := time.Since(start)
+
+			if err != nil {
+				fmt.Printf("request %d failed: %v\n", i+1, err)
+				resultsChannel <- result{statusCode: -1, duration: elapsed}
+				return
+			}
+			defer resp.Body.Close()
+
+			fmt.Printf("Request %d: Status Code: %d (%v)\n", i+1, resp.StatusCode, elapsed)
+
+			resultsChannel <- result{statusCode: resp.StatusCode, duration: elapsed}
+		}(i)
+	}
+
+	wg.Wait()             // wait for all requests to finish
+	close(resultsChannel) // close the results channel to signal completion
+	<-done                // wait for the results collection to finish
+
+	totalDuration := time.Since(startTime)
+
+	report(totalRequests, totalRequestDuration, totalDuration, statusCodeCounter)
+}
+
+func ValidateInputHttpStress(url, method string, totalRequests, concurrentRequests, intervalSec int) error {
+	if url == "" {
+		return fmt.Errorf("URL is required")
+	}
+
+	validMethods := map[string]bool{
+		"GET":    true,
+		"HEAD":   true,
+		"PATCH":  true,
+		"POST":   true,
+		"PUT":    true,
+		"DELETE": true,
+	}
+
+	method = strings.ToUpper(method)
+	if !validMethods[method] {
+		return fmt.Errorf("invalid HTTP method: %s. Allowed methods are: GET, HEAD, PATCH, POST, PUT, DELETE", method)
+	}
+
+	if totalRequests <= 0 {
+		return fmt.Errorf("total requests must be greater than 0")
+	}
+
+	if concurrentRequests <= 0 {
+		return fmt.Errorf("concurrent requests must be greater than 0")
+	}
+
+	if intervalSec < 0 {
+		return fmt.Errorf("interval seconds cannot be negative")
+	}
+
+	return nil
+}
+
+func report(totalRequests int, totalRequestDuration, totalDuration time.Duration, statusCodeCounter map[int]int) {
+	avgDuration := time.Duration(0)
+	if totalRequests > 0 {
+		avgDuration = totalRequestDuration / time.Duration(totalRequests)
+	}
+
+	fmt.Println("\n--- Summary Report ---")
+	fmt.Printf("Total time: %v\n", totalDuration)
+	fmt.Printf("Total requests: %d\n", totalRequests)
+	fmt.Printf("Average response time: %v\n", avgDuration)
+	fmt.Printf("Status code 200: %d response(s)\n", statusCodeCounter[200])
+	for code, count := range statusCodeCounter {
+		if code == 200 {
+			continue
+		}
+
+		fmt.Println("Request with other status codes:")
+		if code == -1 {
+			fmt.Printf("Failed requests: %d\n", count)
+		} else {
+			fmt.Printf("Status code %d: %d response(s)\n", code, count)
+		}
+	}
+}
